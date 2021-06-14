e00014ee5ab68169dce9545f2ebb0c4c93e2c5b0
yteja555 <yteja888@gmail.com>
2021-06-14 09:35:26 +0530

    deleting unwanted serviceworker adresses

--- routes/subscribe.js
+++ routes/subscribe.js
@@ -10,15 +10,32 @@ const router = express.Router();
 
 //@route post/subscribe
 router.post("/",auth, async (req, res) => {
-    console.log('im here in sub')
   let user = req.user;
   try {
-    await User.findByIdAndUpdate(user.id,{"$push":{subscriptions:req.body.subscription}})
-    res.status(200).send("added subscription"); 
+    const modifiedUser = await User.findByIdAndUpdate(user.id,{"$push":{subscriptions:req.body.subscription}},{new:true});
+    const resp ={value:modifiedUser.subscriptions.length};
+    return res.status(200).send(resp); 
   } catch (err) {
     console.log(err.message)
     return res.status(500).send("Internal Server Error");
   }
 });
 
+
+// @dec delete subscription address when user logsout
+// @route delete/subscription
+router.post("/delete",auth,async(req,res)=>{
+  let user = req.user;
+  try{
+    const exUser = await User.findById(user.id);
+    const newSubscriptions = exUser.subscriptions
+    exUser.subscriptions.splice(req.body.value-1,1);
+    await User.findByIdAndUpdate(user.id,{subscriptions:newSubscriptions});
+    res.status(200).send("deleted subscription"); 
+  }
+  catch(err){
+    console.log(err.message)
+    return res.status(500).send("Internal server error");
+  }
+})
 module.exports = router;
